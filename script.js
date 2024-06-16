792ec36440ad7aa9e8729f4579333050915e873f
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2024-06-17 00:46:28 +0530

    added api

---
+++ script.js
@@ -0,0 +1,20 @@
+var subform = document.getElementById("sheetdb-form");
+
+subform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(subform.action, 
+    {
+        method: "POST",
+        body: new FormData(
+        document.getElementById("sheetdb-form")
+        ),
+    })
+    .then((response) => response.json())
+    .then((html) => 
+        {
+            if (confirm("Form submitted successfully! Do you want to go to the index page?")) {
+                window.location.href = "index.html";
+            }
+    }); 
+});
+
